6be3d7d2e77e7cc7b755a731e663f572d13a0e9e
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-01-03 01:42:37 -0800

    Added scoring functionality

--- src/App.js
+++ src/App.js
@@ -14,43 +14,73 @@ import starterFriends from "./friends.json";
  */
 function shuffle(array) {
 
-	var currentIndex = array.length;
-	var temporaryValue, randomIndex;
+  var currentIndex = array.length;
+  var temporaryValue, randomIndex;
 
-	// While there remain elements to shuffle...
-	while (0 !== currentIndex) {
-		// Pick a remaining element...
-		randomIndex = Math.floor(Math.random() * currentIndex);
-		currentIndex -= 1;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
 
-		// And swap it with the current element.
-		temporaryValue = array[currentIndex];
-		array[currentIndex] = array[randomIndex];
-		array[randomIndex] = temporaryValue;
-	}
-	return array;
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
 };
 
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { friends: shuffle(starterFriends) }
+    this.state = {
+      friends: shuffle(starterFriends),
+      currentScore: 0,
+      bestScore: 0
+    };
   }
 
-  guess = (id)=> {
-    //TO DO: figure out if the particular friend has been clicked before
-    this.setState({friends: shuffle(starterFriends)});
+  guess = (id) => {
+    //First, find exactly the right friend within our state array (based on the id of the element they clicked)
+    const friendIndex = this.state.friends.findIndex(eachFriend => eachFriend.id === id);
+    const friendInQuestion = this.state.friends[friendIndex];
+
+    //if we haven't previously clicked this friend, we get a point added to our score!
+    if (!friendInQuestion.isClicked) {
+      friendInQuestion.isClicked = true;
+      let newScore = this.state.currentScore + 1;
+      this.setState({
+        currentScore: newScore,
+        friends: shuffle(starterFriends)
+      });
+    }
+    //Otherwise, it's a repeat...and the game starts over :)
+    else {
+      //first, check if our previous score beat our record
+      const newHiScore = (this.state.currentScore > this.state.bestScore ? this.state.currentScore : this.state.bestScore);
+      //then quickly reset the clicks for everyone
+      starterFriends.forEach(element => {
+        element.isClicked=false;
+      });
+      this.setState({
+        currentScore: 0,
+        bestScore: newHiScore,
+        friends: shuffle(starterFriends)
+      });
+    }
+
   }
 
   render() {
     return (
       <div>
         <Instructions />
-        <ScoreCard />
+        <ScoreCard currentScore={this.state.currentScore} bestScore={this.state.bestScore}/>
         <GameBoard>
           {this.state.friends.map(currentFriend => {
-            return <ClickyImage key={currentFriend.id} friend={currentFriend} guess={this.guess}/>
+            return <ClickyImage key={currentFriend.id} friend={currentFriend} guess={this.guess} />
           })}
         </GameBoard>
         <Footer />
--- src/components/ScoreCard/index.js
+++ src/components/ScoreCard/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Score.css"
 
-function ScoreCard() {
+function ScoreCard(props) {
   return (
     <div className="score-banner container-fluid">
       <div className="container">
         <div className="row d-flex justify-content-start py-2">
-          <h5 className="px-2 text-warning">Current Score: 0</h5>
+          <h5 className="px-2 text-warning">Current Score: {props.currentScore}</h5>
           <h5 className="px-2"> | </h5>
-          <h5 className="px-2">Top Score: 0</h5>
+          <h5 className="px-2">Best Score: {props.bestScore}</h5>
         </div>
       </div>
     </div>
