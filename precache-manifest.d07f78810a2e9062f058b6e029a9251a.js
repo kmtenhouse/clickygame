9fa69c0363b0267c4032e3770d9b1c0c98db4161
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-01-04 22:57:04 -0800

    Updates

---
+++ precache-manifest.d07f78810a2e9062f058b6e029a9251a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "3a93fea265f7dc0d1b53",
+    "url": "/clickygame/static/js/main.3a93fea2.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/clickygame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "3a93fea265f7dc0d1b53",
+    "url": "/clickygame/static/css/main.73661442.chunk.css"
+  },
+  {
+    "revision": "8f628ffb43e65db665a3bea0d5108b27",
+    "url": "/clickygame/index.html"
+  }
+];
