e64b8983eefbf7a1191e4f0cf4fae082e239348d
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-01-04 23:02:39 -0800

    Deploying to github pages

--- src/App.js
+++ src/App.js
@@ -11,14 +11,11 @@ import GameBoard from "./components/GameBoard/";
 import shuffle from "./utils/shuffle.js";
 import getGameItems from "./utils/gamedata/";
 
-//Global variables
-let allGameItems = getGameItems(10);
-
 class App extends React.Component {  
   constructor(props) {
     super(props);
     this.state = {
-      friends: shuffle(allGameItems),
+      friends: getGameItems(10), //default is 10 items in our game
       currentScore: 0,
       bestScore: 0
     };
@@ -35,24 +32,25 @@ class App extends React.Component {
       let newScore = this.state.currentScore + 1;
       this.setState({
         currentScore: newScore,
-        friends: shuffle(allGameItems)
+        friends: shuffle(this.state.friends.slice(0)) 
+        //NOTE: we have to make a shallow copy of the array in order to shuffle, bc the shuffle function directly mutates the array it is operating on. And react doesn't like data in the 'state' to be modified outside of setState. Since slice provides a shallow copy of the original array though, the objects inside the slice still point to the original objects no problem
       });
     }
     //Otherwise, it's a repeat...and the game starts over :)
     else {
       //first, check if our previous score beat our record
+      //if so, make this score our new BEST SCORE
       const newHiScore = (this.state.currentScore > this.state.bestScore ? this.state.currentScore : this.state.bestScore);
       //then quickly reset the clicks for everyone
-      allGameItems.forEach(element => {
+      this.state.friends.forEach(element => {
         element.isClicked=false;
       });
       this.setState({
         currentScore: 0,
         bestScore: newHiScore,
-        friends: shuffle(allGameItems)
+        friends: shuffle(this.state.friends.slice(0))
       });
     }
-
   }
 
   render() {
